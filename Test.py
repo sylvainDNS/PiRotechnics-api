129040fec9f635e78686ad6a6964e5070e30693b
sylvainDNS <sylvain.denyse@gmail.com>
2016-05-14 16:30:57 +0200

    4

--- Test.py
+++ Test.py
@@ -1,24 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT, initial=True)
-GPIO.setup(17, GPIO.OUT, initial=False)
 
-print(GPIO.input(4))
-print(GPIO.input(17))
-
-time.sleep(4)
-
-GPIO.output(4, True)
-GPIO.output(17, False)
-
-print(GPIO.input(4))
-print(GPIO.input(17))
-
-time.sleep(4)
-
-GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.OUT, initial=True)
