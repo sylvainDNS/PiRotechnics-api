feed43195e13a69bac396957672019e67709a8cd
sylvainDNS <sylvain.denyse@gmail.com>
2016-05-14 16:32:47 +0200

    5

--- Test.py
+++ Test.py
@@ -4,7 +4,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT, initial=True)
+GPIO.setup(4, GPIO.OUT, initial=False)
+
+time.sleep(2)
+print(start)
 
 p = GPIO.PWM(4, 0.5)
 p.start(50)
