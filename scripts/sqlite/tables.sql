e9cb875525393779d1b1d43587600ee80f5ed481
Sylvain <sylvain.denyse@gmail.com>
2018-07-09 23:47:01 +0200

    simplify step schema

--- scripts/sqlite/tables.sql
+++ scripts/sqlite/tables.sql
@@ -9,14 +9,9 @@ CREATE TABLE show (
 CREATE TABLE step ( 
     step_id TEXT, 
     show_id TEXT,
-    cueOrder INTEGER not null,
-    name TEXT not null, 
-    channel INT not null,
     time INT not null,
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     updatedAt DATETIME,
-    UNIQUE (show_id, cueOrder),
-    UNIQUE (show_id, channel)
     CONSTRAINT step_pk PRIMARY KEY (step_id),
     CONSTRAINT step_show_fk FOREIGN KEY (show_id) REFERENCES show(show_id) ON DELETE CASCADE
 );
